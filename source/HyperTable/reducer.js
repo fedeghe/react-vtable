1945b25b9e609376e23559ac8cc66d6aa76452cf
fedeghe <fedeghe@gmail.com>
2021-02-14 18:09:15 +0100

    multifilter

--- source/HyperTable/reducer.js
+++ source/HyperTable/reducer.js
@@ -5,11 +5,33 @@ const reducer = (oldState, action) => {
     const { payload, type } = action
     switch (type) {
         case 'sortBy':
-            return [...oldState].sort(payload.sort);
+            return  {
+                ...oldState,
+                rows: oldState.rows.sort(payload.sort)
+            }
         case 'filter': 
-            return [...originalData].filter(
-                row => `${row[payload.field]}`.includes(`${payload.value}`)
-            );
+            const newFilters = {
+                ...oldState.filters,
+                [payload.field]: payload.value
+            }
+            if (payload.value.length === 0) {
+                newFilters[payload.filter] = null;
+                delete newFilters[payload.filter]
+            }
+            // apply all
+            return {
+                filters: newFilters,
+                rows: [...originalData.reduce((acc, row) => {
+                    let canStay = true
+                    for (let f in newFilters) {
+                        if (!(`${row[f]}`.includes(`${newFilters[f]}`))) {
+                            canStay = false
+                        }
+                    }
+                    if (canStay) acc.push(row)
+                    return acc
+                }, [])]
+            }
         default:
             return oldState
     }
