372f84a182b047bdc00407f97f313b1aa8d8da2b
fedeghe <fedeghe@gmail.com>
2021-02-21 10:53:48 +0100

    use web-page-monitor instead

--- source/index.jsx
+++ source/index.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import { startFPSMonitor, startMemMonitor } from 'perf-monitor'
+// import { startFPSMonitor, startMemMonitor } from 'perf-monitor'
 import Playground from './Playground'
+import 'web-page-monitor'
 
 if(location.host.match(/^localhost/)) {
-    startFPSMonitor();
-    startMemMonitor();
+    WebPageMonitor
+        .showNET()
+        .showFPS()
+        .showMEM({
+            height: 30,
+        })
+        .showTAGS({
+            frequency: 10,
+        })
+        .showEVENTS({
+            frequency: 10,
+            exclude: [
+                'onmousemove',
+            ]
+        })
+        .render();
 }
 
 
