b5b5c7ad128635e3daf59841fb55abb60a26fe27
fedeghe <fedeghe@gmail.com>
2022-05-29 20:49:08 +0200

    now

--- source/utils.js
+++ source/utils.js
@@ -1,18 +1,20 @@
 let id = 0,
     strid = 0;
-const generators = {
-        int: () => parseInt(Math.random() * 1000, 10),
-        str: () => `a random string ${++strid}`,
-        date: () => `${new Date()}`,
-        id: () => ++id
+const sameInt = 12345678,
+    sameDate = +new Date(2000,1,1,1,1,1),
+    generators = {
+        int: (same, i) => same ? sameInt + i : parseInt(Math.random() * 1000, 10),
+        str: (same, i) => `a random string ${same ? i : ++strid}`,
+        date: (same, i) => `${same ? new Date(sameDate+i) : new Date()}`,
+        id: (same, i) => `${'ID' + same ? i : ++id}`
     },
     getType = (type, args = []) => 
         type in generators ? generators[type](...args) : 'no type',
 
-    generateRowData = (fields, num) =>
+    generateRowData = (fields, num, same) =>
         Array.from({ length: num }).map(() =>
-            fields.reduce((acc, field) => {
-                acc[field.key] = getType(field.type);
+            fields.reduce((acc, field, i) => {
+                acc[field.key] = getType(field.type, same, i);
                 return acc;
             }, {})
         );
