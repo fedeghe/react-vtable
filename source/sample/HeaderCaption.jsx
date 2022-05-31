25ecccd5b87ea76ade64c95dbeda43af2a851d8b
fedeghe <fedeghe@gmail.com>
2022-05-31 17:55:13 +0200

    Boolean

--- source/sample/HeaderCaption.jsx
+++ source/sample/HeaderCaption.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const HeaderCaption = ({ total, activeColumn, activeRow, scrollTop, unFilter, activeFiltersCount }) => (
     <div style={{ color: 'white', backgroundColor: 'royalBlue', height: 'inherit', fontSize: '1.2em', padding:'10px' }}>
         Pre header component ({total}) [{activeRow}, {activeColumn}] scrollTop: [{scrollTop}]
-        {activeFiltersCount && <button onClick={unFilter}>unFilter all</button>}
+        {Boolean(activeFiltersCount) && <button onClick={unFilter}>unFilter all</button>}
     </div>
 );
 
