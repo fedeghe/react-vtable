eb813366a096a32c8db90f033e9c4a932d6163d5
fedeghe <fedeghe@gmail.com>
2022-05-31 08:51:01 +0200

    Wrapper is on the user

--- source/HyperTable/style.js
+++ source/HyperTable/style.js
@@ -8,20 +8,4 @@ export default createUseStyles(() => ({
         overflow: 'hidden',
         position:'relative'
     }),
-    Loading: {
-        position:'absolute',
-        top: 0,
-        left: 0,
-        width: `100%`,
-        height: `100%`,
-        display:'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        opacity:0.7,
-        backgroundColor: 'white',
-        letterSpacing: 2,
-        pointerEvents: 'none',
-        zIndex:1000,
-        textTransform:'uppercase'
-    }
 }));
