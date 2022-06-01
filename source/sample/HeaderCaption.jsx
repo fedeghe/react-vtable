0f9cb11baa0599515a1946fe3adc40dbf83acb18
fedeghe <fedeghe@gmail.com>
2022-06-01 23:53:08 +0200

    loading passed

--- source/sample/HeaderCaption.jsx
+++ source/sample/HeaderCaption.jsx
@@ -4,13 +4,16 @@ const HeaderCaption = ({
     total, activeColumn, activeRow, scrollTop,
     unFilter,
     unSort,
-    activeFiltersCount,
-    isSorting
+    // activeFiltersCount,
+    isSorting,
+    isFiltering,
+    loading
 }) => (
     <div style={{ color: 'white', backgroundColor: 'royalBlue', height: 'inherit', fontSize: '1.2em', padding:'10px' }}>
         Pre header component ({total}) [{activeRow}, {activeColumn}] scrollTop: [{scrollTop}]
-        {Boolean(activeFiltersCount) && <button onClick={unFilter}>unFilter all</button>}
+        {Boolean(isFiltering) && <button onClick={unFilter}>unFilter all</button>}
         {Boolean(isSorting) && <button onClick={unSort}>unSort all</button>}
+        {loading && <span> LOADING</span>}
     </div>
 );
 
