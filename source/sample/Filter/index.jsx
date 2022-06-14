50411f85d3c8082d71bcd1e896328101b9a28099
fedeghe <fedeghe@gmail.com>
2022-06-14 20:40:19 +0200

    better sample autofocus on open

--- source/sample/Filter/index.jsx
+++ source/sample/Filter/index.jsx
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useStyles from './style.js';
 
 const Filter =  ({filter}) => {
     const classes = useStyles(),
-        {value, setValue, visibility, setVisibility, unFilter} = filter;
+        {value, setValue, visibility, setVisibility, unFilter} = filter,
+        [human, setHuman] = useState(false);
 
     return <div className={classes.Container}>{
         visibility
         ? <div>
-            <input type="text" onChange={e => setValue(e.target.value.replace(/^\s/g, ''))} value={value} autoFocus/>
+            <input type="text" onChange={e => setValue(e.target.value.replace(/^\s/g, ''))} value={value} autoFocus={human}/>
             <span className={classes.Pointer} onClick={() => {
                 setValue('');
+                setHuman(false);
                 setVisibility(false);
             }}>&times;</span>
             &nbsp;|&nbsp;
             <span className={classes.Pointer} onClick={unFilter}>unfilter all</span>
         </div>
-        : <span className={classes.Pointer} onClick={() => setVisibility(true)}>Y</span>
+        : <span className={classes.Pointer} onClick={() => {
+            setHuman(true);
+            setVisibility(true);
+        }}>Y</span>
     }</div>;
 };
 export default Filter;
