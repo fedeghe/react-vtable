1b43d9494bc1a874e1ed86829fb08ae2fbc3583b
fedeghe <fedeghe@gmail.com>
2022-06-15 21:29:03 +0200

    just readable

--- source/sample/FooterCaption.jsx
+++ source/sample/FooterCaption.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const FooterCaption = ({ from, to, activeColumnIndex, activeRowIndex, filtered, total }) => (
-    <div style={{ color: 'white', backgroundColor: 'royalBlue', height: 'inherit', textAlign: 'center' }}>Post footer component [{from}, {to}] [row: {activeRowIndex}, col: {activeColumnIndex}] filtered {filtered} out of {total}</div>
+    <div style={{ color: 'white', backgroundColor: 'royalBlue', height: 'inherit', textAlign: 'center' }}>
+        Post footer component [{from}, {to}] [row: {activeRowIndex}, col: {activeColumnIndex}] filtered {filtered} out of {total}
+    </div>
 );
 
 export default FooterCaption;
