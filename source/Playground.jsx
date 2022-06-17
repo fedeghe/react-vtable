2e6accbab99fe02ad67715e2c452717e0fbb1051
fedeghe <fedeghe@gmail.com>
2022-06-17 09:04:47 +0200

    use small by default

--- source/Playground.jsx
+++ source/Playground.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import HyperTable from './HyperTable';
 
-import config from './configBig';
-// import config from './configSmall';
+// import config from './configBig';
+import config from './configSmall';
 
 import './sample/user.css';
 
