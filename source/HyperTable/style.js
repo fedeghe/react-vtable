8ac96c8fbc3c7f1c4245bf38b3e77e06ffb9a186
fedeghe <fedeghe@gmail.com>
2022-06-20 22:57:20 +0200

    remove some inline styles

--- source/HyperTable/style.js
+++ source/HyperTable/style.js
@@ -8,4 +8,11 @@ export default createUseStyles({
         overflow: 'hidden',
         position:'relative'
     }),
+    LoaderContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%'
+    }
 });
