c73cec2578f6737d8e2ac670554983faabac55dd
fedeghe <fedeghe@gmail.com>
2022-07-04 07:48:55 +0200

    also add user.css

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import './../source/sample/user.css'
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
