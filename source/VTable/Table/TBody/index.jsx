dba86f67bfaf1bc6b0842c42ddf05b0d865affbc
fedeghe <fedeghe@gmail.com>
2022-11-23 09:19:36 +0100

    useCallback

--- source/VTable/Table/TBody/index.jsx
+++ source/VTable/Table/TBody/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 import TableContext from '../../Context';
 import Filler from '../Filler';
 import LeftMost from '../LeftMost';
@@ -34,49 +34,58 @@ export default () => {
                 rhtID
             },
         } = useContext(TableContext),
-        classes = useStyles({ rowHeight });
+        classes = useStyles({ rowHeight }),
+        getTdContent = useCallback(({row, rowIndex}) =>
+            (header, headerIndex) => {
+                let content = row[header.key] || 'nothing';
+                if (header.cell && isFunction(header.cell)) {
+                    content = header.cell({row, header, rowIndex, headerIndex});
+                }
+                if (!header.isVisible)
+                    content = header.removedContent || commonRemovedContent || '';
+                return (
+                    <Td
+                        wrapperStyle={header.isVisible ? {width: `${header.width}px`} : {}}
+                        key={`cell_${row[rhtID]}_${headerIndex}`}
+                        row={row}
+                        header={header}
+                        rowIndex={rowIndex}
+                        headerIndex={headerIndex}
+                        cls={[
+                            classes.AlTop,
+                            activeColumn === header.key ? (crossHighlightClass || columnHighlightClass) : '',
+                            (cellHightlightClass && activeRow === row[rhtID] && activeColumn === header.key) ? cellHightlightClass : ''
+                        ].join(' ')}
+                    >{content}</Td>
+                );
+            },
+            [activeColumn, activeRow, cellHightlightClass, classes.AlTop, columnHighlightClass, commonRemovedContent, crossHighlightClass, rhtID]
+        ),
+        getLRmostClasses = useCallback(
+            ({row, dirClass}) => `${classes.TbodyThMost} ${dirClass} ${classes.AlTop} ${activeRow === row[rhtID] ? (crossHighlightClass || rowHighlightClass) : ''}`,
+            [activeRow, classes.AlTop, classes.TbodyThMost, crossHighlightClass, rhtID, rowHighlightClass]
+        ),
+        getTrContent = useCallback((row, rowIndex) => (
+            <Tr
+                cls={`${activeRow === row[rhtID] ? (crossHighlightClass || rowHighlightClass || "") : ''}`}
+                key={row[rhtID]}
+            >
+                <LeftMost
+                    cls={getLRmostClasses({row, dirClass: classes.TbodyThLeftMost})}
+                    opts={{ row, rowIndex: rowIndex + fromRow, type: 'body' }}
+                />
+                {headers.map(getTdContent({row, rowIndex}))}
+                <RightMost
+                cls={getLRmostClasses({row, dirClass: classes.TbodyThRightMost})}
+                    opts={{ row, rowIndex: rowIndex + fromRow, type: 'body'}}
+                />
+            </Tr>
+        ), [activeRow, classes.TbodyThLeftMost, classes.TbodyThRightMost, crossHighlightClass, fromRow, getLRmostClasses, getTdContent, headers, rhtID, rowHighlightClass]);
 
     return (
         <tbody>
             <Filler {...{ height: headerFillerHeight, colspan, leftMost }} />
-            {rows.map((row, rowIndex) => (
-                <Tr
-                    cls={`${activeRow === row[rhtID] ? (crossHighlightClass || rowHighlightClass || "") : ''}`}
-                    key={row[rhtID]}
-                >
-                    <LeftMost
-                        cls={`${classes.TbodyThMost} ${classes.TbodyThLeftMost} ${classes.AlTop} ${activeRow === row[rhtID] ? (crossHighlightClass || rowHighlightClass) : ''}`}
-                        opts={{ row, rowIndex: rowIndex + fromRow, type: 'body' }}
-                    />
-                    {headers.map((header, headerIndex) => {
-                        let content = row[header.key] || 'nothing';
-                        if (header.cell && isFunction(header.cell)) {
-                            content = header.cell({row, header, rowIndex, headerIndex});
-                        }
-                        if (!header.isVisible)
-                            content = header.removedContent || commonRemovedContent || '';
-                        return (
-                            <Td
-                                wrapperStyle={header.isVisible ? {width: `${header.width}px`} : {}}
-                                key={`cell_${row[rhtID]}_${headerIndex}`}
-                                row={row}
-                                header={header}
-                                rowIndex={rowIndex}
-                                headerIndex={headerIndex}
-                                cls={[
-                                    classes.AlTop,
-                                    activeColumn === header.key ? (crossHighlightClass || columnHighlightClass) : '',
-                                    (cellHightlightClass && activeRow === row[rhtID] && activeColumn === header.key) ? cellHightlightClass : ''
-                                ].join(' ')}
-                            >{content}</Td>
-                        );
-                    })}
-                    <RightMost
-                        cls={`${classes.TbodyThMost} ${classes.TbodyThRightMost} ${classes.AlTop} ${activeRow === row[rhtID] ? (crossHighlightClass || rowHighlightClass) : ''}`}
-                        opts={{ row, rowIndex: rowIndex + fromRow, type: 'body'}}
-                    />
-                </Tr>
-            ))}
+            {rows.map(getTrContent)}
             <Filler {...{ height: footerFillerHeight, colspan, rightMost }} />
         </tbody>
     );
--- source/VTable/Table/Tr/index.jsx
+++ source/VTable/Table/Tr/index.jsx
@@ -4,15 +4,15 @@ import useStyles from './style.js';
 
 export default ({cls, children}) => {
     const {
-        state: {
-            cls: { 
-                elements: {
-                    rowClass,
-                },
+            state: {
+                cls: { 
+                    elements: {
+                        rowClass,
+                    },
+                }
             }
-        }
-    } = useContext(TableContext),
-    classes = useStyles();
+        } = useContext(TableContext),
+        classes = useStyles();
     return <tr className={[classes.Tr, cls, rowClass].join(' ')}>{children}</tr>;
 };
 
